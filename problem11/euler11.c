b1ebe470b952bba5ccfa60992f0dc4d9ea2b3085
rplu1618 <rplu1618@student.liberty.k12.mo.us>
2014-12-04 17:09:57 -0800

    Decided against using fgetc

--- problem11/euler11.c
+++ problem11/euler11.c
@@ -1,33 +1,51 @@
 #include <stdio.h>
 
 //This will get the size of a grid of numbers in a txt file.
-int * fillArray(int xsize, int ysize, FILE *fp)
+int fillArray(int xsize, int ysize, FILE *fp)
 {
-	int toFill[xsize][ysize], xpos, ypos, current, last;
+	int toFill[xsize][ysize], xpos, ypos, current, last, gridpos;
+	char grid[((xsize * ysize)*2)+1];
+
+	fgets(grid, ((xsize * ysize)*2)+1, (FILE*)fp);
 
 	for(ypos = 0; ypos <= ysize;)
 	{
-		for(xpos = 0; xpos <= xsize; &fp++)
+		last = 0;
+		for(xpos = 0; xpos <= xsize; gridpos++)
 		{
-			current = fgetc(&fp)
-			//This part needs to convert a something like 08 to and int of 8
-			if(last != -1 && current != -1)
+			current = grid[gridpos];
+
+			if(last != '\n' && current != '\n' && last != ' ' && current != ' ')
 			{
-				toFill[xpos][ypos] = last * 10 + (current - '0');
+				toFill[xpos][ypos] = ((last - '0') * 10) + (current - '0');
 				xpos++;
 			}
-			if(current != '\n')
-			{
-				if(current == '\0')
-					last = -1;
-				if(current - '0' < 10 && current - '0' >= 0)
-					last = current - '0';
-			} else
+			if(current == '\n')
 			{
 				ypos++;
 				break;
 			}
+			last = current;
 		}
 	}
+
 	return toFill;
+}
+
+int main()
+{
+	FILE *gridfp;
+	gridfp = fopen("grid.txt", "r");
+	int grid;
+	grid = fillArray(20, 20, &gridfp);
+
+	//This is for testing the array filler
+	int i, j;
+	for(i = 0; i <= 20; i++)
+	{
+		for(j = 0; j <= 20; j++)
+		{
+			printf("%d", grid[j][i]);
+		}
+	}
 }

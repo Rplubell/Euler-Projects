328bacd15e3b558f6a869cf6f06f38e3a9e19e38
rplu1618 <rplu1618@student.liberty.k12.mo.us>
2014-12-05 08:44:08 -0800

    Trying this because all errors are from passing to function

--- problem11/euler11.c
+++ problem11/euler11.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-//This will put .txt file into a 2d array
+/*//This will put .txt file into a 2d array
 int fillArray(int *toFill[][20], int xsize, int ysize, FILE *fp)
 {
 	int xpos, ypos, current, last, gridpos;
@@ -30,14 +30,39 @@ int fillArray(int *toFill[][20], int xsize, int ysize, FILE *fp)
 	}
 
 	return 1;
-}
+}*/
 
 int main()
 {
 	FILE *gridfp;
 	gridfp = fopen("grid.txt", "r");
-	int grid[20][20];
-	fillArray(*grid[][20], 20, 20, gridfp);
+	int toFill[20][20];
+	//fillArray(*grid, 20, 20, gridfp);
+
+	int xpos, ypos, current, last, gridpos, ysize, xsize;
+	ysize = xsize = 20;
+	char grid[((xsize * ysize)*2)+1];
+        fgets(grid, ((xsize * ysize)*2)+1, (FILE*)gridfp);
+        for(ypos = 0; ypos <= ysize;)
+        {
+                last = 0;
+                for(xpos = 0; xpos <= xsize; gridpos++)
+                {
+                        current = grid[gridpos];
+
+                        if(last != '\n' && current != '\n' && last != ' ' && current != ' ')
+                        {
+                                toFill[xpos][ypos] = ((last - '0') * 10) + (current - '0');
+                                xpos++;
+                        }
+                        if(current == '\n')
+                        {
+                                ypos++;
+                                break;
+                        }
+                        last = current;
+                }
+        }
 
 	//This is for testing the array filler
 	int i, j;

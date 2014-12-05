209c21f338903c6357264a17e13cd7c9c1e55bf7
rplu1618 <rplu1618@student.liberty.k12.mo.us>
2014-12-05 09:08:05 -0800

    No errors but still not working properly.

--- problem11/euler11.c
+++ problem11/euler11.c
@@ -9,38 +9,49 @@ int fillArray(int *toFill[][20], int xsize, int ysize, FILE *fp)
 	fgets(grid, ((xsize * ysize)*2)+1, (FILE*)fp);
 
 	for(ypos = 0; ypos <= ysize;)
-	{
-		last = 0;
-		for(xpos = 0; xpos <= xsize; gridpos++)
-		{
-			current = grid[gridpos];
+char grid[((xsize * ysize)*2)+1];
 
-			if(last != '\n' && current != '\n' && last != ' ' && current != ' ')
-			{
-				toFill[xpos][ypos] = ((last - '0') * 10) + (current - '0');
-				xpos++;
-			}
-			if(current == '\n')
-			{
-				ypos++;
-				break;
-			}
-			last = current;
-		}
-	}
+        fgets(grid, ((xsize * ysize)*2)+1, (FILE*)fp);
 
-	return 1;
-}*/
+        for(ypos = 0; ypos <= ysize;)
+        {
+                last = 0;
+                for(xpos = 0; xpos <= xsize; gridpos++)
+                {
+                        current = grid[gridpos];
+
+                        if(last != '\n' && current != '\n' && last != ' ' && current != ' ')
+                        {
+                                toFill[xpos][ypos] = ((last - '0') * 10) + (current - '0');
+                                xpos++;
+                        }
+                        if(current == '\n')
+                        {
+                                ypos++;
+                                break;
+                        }
+                        last = current;
+                }
+        }
+
+        return 1;	{
+*/
 
 int main()
 {
 	FILE *gridfp;
 	gridfp = fopen("grid.txt", "r");
+	if(!gridfp)
+	{
+		printf("Nuuuu");
+		return 0;
+	}
 	int toFill[20][20];
 	//fillArray(*grid, 20, 20, gridfp);
 
 	int xpos, ypos, current, last, gridpos, ysize, xsize;
 	ysize = xsize = 20;
+	gridpos = 0;
 	char grid[((xsize * ysize)*2)+1];
         fgets(grid, ((xsize * ysize)*2)+1, (FILE*)gridfp);
         for(ypos = 0; ypos <= ysize;)
@@ -70,7 +81,7 @@ int main()
 	{
 		for(j = 0; j <= 20; j++)
 		{
-			printf("%d", grid[j][i]);
+			printf("%d ", toFill[j][i]);
 		}
 	}
 }

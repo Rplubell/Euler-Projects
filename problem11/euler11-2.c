c3993ef8c20091c9ae321b60acd31b9e95b76b0d
rplu1618 <rplu1618@student.liberty.k12.mo.us>
2014-12-05 12:48:41 -0800

    .txt to 2d array currently working, need to implement direction functions now

--- problem11/euler11-2.c
+++ problem11/euler11-2.c
@@ -15,19 +15,29 @@ int main()
 	}
 
 	char buffer[BUFFERSIZE];
-	int value;
+	int value, toFill[20][20], ypos, xpos;
+	char* ptr;
 
-	while(fgets(buffer, sizeof(buffer), fp)!=NULL)
+	//printf("%s \n", buffer);
+	for(ypos = 0; fgets(buffer, sizeof(buffer), fp)!= NULL && ypos <= 20; ypos++)
 	{
-		printf("%s \n", buffer);
-		char* ptr = strtok(buffer," ");
-		while(ptr)
+		ptr = strtok(buffer, " ");
+		for(xpos = 0; ptr != 0 && xpos <= 20; xpos++)
 		{
 			printf("token string %s\n", ptr);
-			value = atoi(ptr);
-			printf("value: %d\n", value);
+			toFill[ypos][xpos] = atoi(ptr);
+			printf("value: %d\n", toFill[ypos][xpos]);
+			printf("xpos %d ypos %d\n", xpos, ypos);
 			ptr = strtok(NULL, " ");
 		}
 	}
+
 	fclose(fp);
+
+	int i, x;
+	for(i = 0; i < 20; i++)
+	{
+		for(x = 0; x < 20; x++)
+			printf("%d\n", toFill[i][x]);
+	}
 }
